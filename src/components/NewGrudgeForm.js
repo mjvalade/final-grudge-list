ffd17f628ecef4b865cb3aa5ddbdddee5eb23b67
mjvalade <mjvalade@gmail.com>
2017-01-10 12:08:39 -0700

    built NewGrudgeForm component

--- src/components/NewGrudgeForm.js
+++ src/components/NewGrudgeForm.js
@@ -5,7 +5,38 @@ class NewGrudgeForm extends Component {
   render() {
     return(
       <section className="new-form">
-        <h2 className="new-form-title">Add yet another grudge to yer list!</h2>
+        <h1 className="new-form-title">Add yet another grudge to yer list!</h1>
+        <form className="input-form">
+          <label>
+            Name:
+            <input
+              type="text"
+              name="title"
+              className="new-input new-name"
+              placeholder="Enter name"
+            />
+          </label>
+          <label>
+            What they did:
+            <textarea
+              type="text"
+              name="title"
+              className="new-input reason"
+              placeholder="Why should they go play in traffic?"
+            />
+          </label>
+          <article className="buttons">
+            <input
+              type="submit"
+              className="save-button"
+              value="Save"
+            />
+            <input
+              className="cancel-button"
+              value="Cancel"
+            />
+          </article>
+        </form>
       </section>
     )
   }

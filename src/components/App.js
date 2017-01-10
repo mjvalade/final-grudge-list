196c9ee51991902a3ce62d1e8cb935193ef9dd43
mjvalade <mjvalade@gmail.com>
2017-01-10 10:45:29 -0700

    changed file structure, connected Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import './styles/css/index.css';
+import Header from './Header';
+import '../styles/css/index.css';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Final Grudge List</h2>
-        </div>
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return(
+      <header className="header">
+        <h2 className="header-title">Final Grudge List</h2>
+      </header>
+    )
+  }
+}
+
+export default Header;

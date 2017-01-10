ca8fff5f1608fcc9765b8edfeff2c5f37ec8e762
mjvalade <mjvalade@gmail.com>
2017-01-10 10:02:58 -0700

    initial commit, set up create-react-app and local sass files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './styles/css/index.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Final Grudge List</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
